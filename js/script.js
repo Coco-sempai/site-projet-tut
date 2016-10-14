e73de2a31daa8744523aa0f875c6c0555f83164d
ThorXG <thorxgstudio@gmail.com>
2016-10-14 13:33:15 +0200

    fin du test JS

--- js/script.js
+++ js/script.js
@@ -1,6 +1,9 @@
-setTimeout(function(){ 
-	$('.main-title').typist({
-	    speed: 8,
-	    text: 'Projet tuteuré \n Le business lucratif des \n failles informatiques'
-	});
-}, 1500);
+$(document).ready(function() {
+	//displayMainTitle();
+	setTimeout(function(){ 
+		$('.main-title').typist({
+	        speed: 8,
+	        text: 'Projet tuteuré \n Le business lucratif des \n failles informatiques'
+	    });
+	}, 1500);
+});
