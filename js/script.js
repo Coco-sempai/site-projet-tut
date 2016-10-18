6d20a577c72f1673f3e5ef6f2163d8b311da8827
ThorXG <thorxgstudio@gmail.com>
2016-10-18 09:05:05 +0200

    Animation Slack

--- js/script.js
+++ js/script.js
@@ -1,9 +1,12 @@
 $(document).ready(function() {
-	//displayMainTitle();
 	setTimeout(function(){ 
 		$('.main-title').typist({
 	        speed: 12,
 	        text: 'Projet tuteuré \n Le business lucratif des \n failles informatiques'
 	    });
 	}, 1500);
+
+	setTimeout(function(){ 
+		$('svg').addClass('animated');
+	}, 7000);
 });
