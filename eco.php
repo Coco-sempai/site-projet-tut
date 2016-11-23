db6a596c131bbd1cde71334b1c743d7a870b346a
nathan71370 <nathanmercier@orange.fr>
2016-11-23 16:15:57 +0100

    Partie eco Nathan

--- eco.php
+++ eco.php
@@ -9,7 +9,17 @@ include('includes/nav.php');
 </script>
 
 <main>
-	
+	<div class="col s12 m4 l4 col-subject">
+          <div class="icon-block">
+            <h2 class="center brown-text"><i class="material-icons">trending_up</i></h2>
+            <h5 class="center">Partie économique et juridique</h5>
+
+            <p class="eco">Si on remonte quelques années en arrière, les hackers prévenaient les fabricants logiciels à la découverte d’une faille. Les efforts fournis par l’attaquant étaient donc gratuits et la seule reconnaissance possible de son travail se faisait par le fabricant logiciel. Celui-ci ne corrigeait d’ailleurs pas toujours ses vulnérabilités (souvenons nous de Microsoft et sa faille MS08-028  qu’il a mis 1138 jours à corriger – soit plus de 3 ans).
+Puis est venu le temps du fulldiscolure, entendez par là « publication d’une vulnérabilité au grand publique sans prévenir le fabricant logiciel ». L’objectif premier était de faire pression sur les fabricants pour qu’ils corrigent la vulnérabilité en question.
+Nous observons une nouvelle tendance, plus commerciale mais aussi moins éthiquement correcte. A la découverte d’une faille le hacker contact le fabricant pour lui proposer les fruits de son travail contre rémunération. Dans le cas où l’entreprise n’est pas d’accord avec la condition de l’attaquant nous risquons de voir quelques jours plus tard la faille publiée à une vente aux enchères à 0day.</p>
+          </div>
+        </a>
+      </div>
 </main>
 
 <?php
