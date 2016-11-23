99bfe098a767d7727c8066a928ce48d21891dcfc
nathan71370 <nathanmercier@orange.fr>
2016-11-23 14:09:40 +0100

    membre v3

--- membres.php
+++ membres.php
@@ -29,7 +29,7 @@ include('includes/nav.php');
           </div>
 
           <p class="valign">Thomas, 19ans, alias "Chef du groupe". Thomas, lui aussi ancien de l'UTBM, est un très bon coordinateur et à  réussi à gérer correctement ce projet de  premier semestre à l'IUT de Belfort-Montbéliard.
-          Thomas s'interesse tout particulièrement au développement mobile, et travaille sur sa première application IOS: "Near". Celle ci est actuellement en test sur l'App Store. Il espère continuer ses études après son DUT.</p>
+          Thomas s'interesse tout particulièrement au développement mobile, et travaille sur sa première application IOS: "<em>Near</em>". Celle ci est actuellement en test sur l'<em>App Store</em> d'<em>Apple</em>. Il espère continuer ses études après son DUT.</p>
         </div>
       </div>
     </div>
@@ -46,7 +46,7 @@ include('includes/nav.php');
             <h5 class="hide-on-small-only right-align">Maxime LUCAS</h5>
           </div>
           
-          <p class="valign">Maxime, 20 ans et toujours aussi Geek. Maxime est un ancien de l'UTBM qui à préféré étudier à l'IUT de Belfort-Montbéliard. Geek dans l'âme, vous pourrez trouver facilement des vidéos de ses parties sur les sites de vidéo les plus réputés. Aujourd'hui, à coté de ses études, il travaille activement sur un projet de site Internet dédié aux jeux-vidéos. Toujours serviable, vous pourrez le retrouver au bar si vous avez besoin d'aide.</p>
+          <p class="valign">Maxime, 20 ans et toujours aussi <em>Geek</em>. Maxime est un ancien de l'UTBM qui à préféré étudier à l'IUT de Belfort-Montbéliard. <em>Geek</em> dans l'âme, vous pourrez trouver facilement des vidéos de ses parties sur les sites de vidéo les plus réputés. Aujourd'hui, à coté de ses études, il travaille activement sur un projet de site Internet dédié aux jeux-vidéos. Toujours serviable, vous pourrez le retrouver au bar si vous avez besoin d'aide.</p>
         </div>
       </div>
 
@@ -72,7 +72,7 @@ include('includes/nav.php');
           </div>
 
           <p class="valign">Titulaire d'un Baccalauréat S, Thomas est un Alsacien de 18 ans (et toutes ses dents). Toujours de bon humeur et toujours prêt à donner un coup de main, il est très facile de travailler avec lui. Il est pluôt perfectionniste et patient mais à la condition que le domaine l'intéresse.
-          Il espère s'orienter vers des études tournés vers la sécurité après son DUT. En attendant vous pouvez le retrouver régulièrement sur de_dust2.</p>
+          Il espère s'orienter vers des études tournés vers la sécurité après son DUT. En attendant vous pouvez le retrouver régulièrement sur <em>de_dust2</em>.</p>
         </div>
       </div>
     </div>
@@ -89,7 +89,7 @@ include('includes/nav.php');
             <h5 class="hide-on-small-only right-align">Corentin DE OLIVEIRA</h5>
           </div>
           
-          <p class="valign">Corentin 21ans, recherche activement coéquipière. Corentin s'est réorienté en informatique après avoir suivi 2ans d'études en STAPS. Sportif, il pratique régulièrement du Tennis avec ses amis. Fasciné lui aussi par les jeux-vidéos, il espère pouvoir continuer ses études dans ce domaine. (Encore une fois une coéquipière serait appréciée !) </p>
+          <p class="valign">Corentin 21ans, recherche activement coéquipière. Corentin s'est réorienté en informatique après avoir suivi 2ans d'études en STAPS. Sportif, il pratique régulièrement du Tennis avec ses amis (et des tournois de bras de fer). Fasciné lui aussi par les jeux-vidéos, il espère pouvoir continuer ses études dans ce domaine. (Encore une fois une coéquipière serait appréciée !) </p>
         </div>
       </div>
 
@@ -114,7 +114,7 @@ include('includes/nav.php');
             <h5 class="hide-on-small-only">Nathan MERCIER</h5>
           </div>
 
-          <p class="valign">Nathan, 18 ans, tituaire d'un Baccalauréat STI2D. Nathan est passionné par la magie et passe pas mal de son temps libre à trouver des tours de magie sur Internet. Nathan espère continuer ses études pour devenir Ingénieur Informaticien. D'ici là peut être que vous pourrez apprécier ses nombreux tours de magie ! </p>
+          <p class="valign">Nathan, 18 ans, tituaire d'un Baccalauréat STI2D. Nathan est passionné par la magie et passe pas mal de son temps libre à trouver des tours de magie sur Internet. Nathan espère continuer ses études pour devenir Ingénieur Informaticien. D'ici là, peut être que vous pourrez apprécier ses nombreux tours de magie ! </p>
         </div>
       </div>
     </div>
