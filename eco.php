03783d46c5ac735e1a0d77ca1a3152fb2f06ac01
nathan71370 <nathanmercier@orange.fr>
2016-11-23 16:29:13 +0100

    modifeco3

--- eco.php
+++ eco.php
@@ -14,56 +14,57 @@ include('includes/nav.php');
             <h2 class="center brown-text"><i class="material-icons">trending_up</i></h2>
             <h5 class="center">Partie économique et juridique</h5>
 
-            <p class="eco">Si on remonte quelques années en arrière, les hackers prévenaient les fabricants logiciels à la découverte d’une faille. Les efforts fournis par l’attaquant étaient donc gratuits et la seule reconnaissance possible de son travail se faisait par le fabricant logiciel. Celui-ci ne corrigeait d’ailleurs pas toujours ses vulnérabilités (souvenons nous de Microsoft et sa faille MS08-028  qu’il a mis 1138 jours à corriger – soit plus de 3 ans).
+            <p class="eco"><br/><br/>Si on remonte quelques années en arrière, les hackers prévenaient les fabricants logiciels à la découverte d’une faille. Les efforts fournis par l’attaquant étaient donc gratuits et la seule reconnaissance possible de son travail se faisait par le fabricant logiciel. Celui-ci ne corrigeait d’ailleurs pas toujours ses vulnérabilités (souvenons nous de Microsoft et sa faille MS08-028  qu’il a mis 1138 jours à corriger – soit plus de 3 ans).
 Puis est venu le temps du fulldiscolure, entendez par là « publication d’une vulnérabilité au grand publique sans prévenir le fabricant logiciel ». L’objectif premier était de faire pression sur les fabricants pour qu’ils corrigent la vulnérabilité en question.
-Nous observons une nouvelle tendance, plus commerciale mais aussi moins éthiquement correcte. A la découverte d’une faille le hacker contact le fabricant pour lui proposer les fruits de son travail contre rémunération. Dans le cas où l’entreprise n’est pas d’accord avec la condition de l’attaquant nous risquons de voir quelques jours plus tard la faille publiée à une vente aux enchères à 0day.
+Nous observons une nouvelle tendance, plus commerciale mais aussi moins éthiquement correcte. A la découverte d’une faille le hacker contact le fabricant pour lui proposer les fruits de son travail contre rémunération. Dans le cas où l’entreprise n’est pas d’accord avec la condition de l’attaquant nous risquons de voir quelques jours plus tard la faille publiée à une vente aux enchères à 0day.<br/>
 Travaillant d’une manière plus conventionnelle ces plateformes proposent de grosse prime pour la recherche de failles sur un environnement particulier. Elles se chargent ensuite de contacter les agences gouvernementales et entreprises afin de pouvoir s’en protéger. Les primes offertes pour ce genre d’actions sont très importantes :
 
 
+<br/>
 
-
-
+<br/>
 L’entreprise Zerodium a, l’année dernière, proposer un million d’euro à qui trouvera une faille informatique dans iOS 9, le logiciel qui équipe les iPhones. Zerodium est une nouvelle venue sur le marché de la sécurité informatique, mais pas une inconnue. Emanation de l’entreprise française Vupen, elle est spécialisée dans les failles dites « zero day », qu’elle achète à des hackers puis revend au plus offrant, le plus souvent de grandes agences de renseignement occidentales qui cherchent un moyen d’espionner leurs cibles – Vupen, puis Zerodium, ont affirmé ne pas travailler avec des régimes autoritaires.
-
-
+<br/>
+<br/>
 Microsoft propose différents tarifs en fonction de la nature et de la dangerosité de la faille. A l’origine de ce programme, une chercheuse réputée en cybersécurité, Katie Moussouris. Elle a travaillé pendant sept ans chez Microsoft, où sa principale tâche était de trouver des moyens de collaborer avec la communauté des hackers.
+<br/>
 « J’ai commencé à creuser la piste des primes en 2010, et j’ai créé des modèles qui pouvaient être utilisés par Microsoft, notamment parce que c’est une vieille entreprise avec de nombreux produits différents et qu’on ne pouvait pas juste partir en se disant qu’on mettrait des primes pour tous les bugs. »
-Aujourd’hui, Katie Moussouris ne travaille plus pour Microsoft, mais est directrice des affaires publiques pour HackerOne. Fondée en 2012 par plusieurs experts en sécurité informatique, cette startup de San Francisco compte dans ses rangs des anciens de Symantec et de Facebook. Elle met en relation les entreprises et les hackers susceptibles de trouver des vulnérabilités. L’objectif d’HackerOne est d’encourager ceux qui découvrent des failles de sécurité à se diriger vers l’entreprise pour signaler leur trouvaille. En simplifiant ce processus, l’entreprise limite le risque d’attaques. Parmi ses clients, on trouve notamment Snapchat, Twitter, Yahoo, Dropbox et Airbnb.
+Aujourd’hui, Katie Moussouris ne travaille plus pour Microsoft, mais est directrice des affaires publiques pour HackerOne. Fondée en 2012 par plusieurs experts en sécurité informatique, cette startup de San Francisco compte dans ses rangs des anciens de Symantec et de Facebook. Elle met en relation les entreprises et les hackers susceptibles de trouver des vulnérabilités. L’objectif d’HackerOne est d’encourager ceux qui découvrent des failles de sécurité à se diriger vers l’entreprise pour signaler leur trouvaille. En simplifiant ce processus, l’entreprise limite le risque d’attaques. Parmi ses clients, on trouve notamment Snapchat, Twitter, Yahoo, Dropbox et Airbnb.<br/>
 « Certains de nos clients ont des programmes de primes, d’autres non, mais ils utilisent tous notre plateforme pour mieux gérer et traiter ce qui leur arrive de la communauté hacker. Nous faisons cela pour aider les équipes d’intervention à avoir les meilleurs rapports possibles sur les failles. »
 HackerOne a également bâti un système de réputation pour les hackers. En clair, si un expert envoie plusieurs rapports de vulnérabilité avérés sur la plateforme, ses signalements pourront être traités en priorité par les entreprises. Les hackers les plus réputés sont également invités à participer à des programmes sur invitation.
 
-
-Puisque ces failles sont si précieuses, les entreprises ont tout intérêt à les trouver elles-mêmes. Certaines ont mis en place des modules internes pour traquer les failles. En juillet 2014, Google a lancé Project Zero, une équipe alors dirigée par le « monsieur sécurité » du navigateur Chrome, Chris Evans (qui est aujourd’hui consultant pour HackerOne). Des hackers et spécialistes en sécurité informatique ont pour tâche de traquer les vulnérabilités qui peuvent se cacher dans les produits de Google mais aussi d’autres services. Chaque faille découverte est alors notifiée à l’entreprise concernée, et signalée sur le blog de Project Zero une fois qu’elle a été réparée. L’équipe a notamment compté dans ses rangs le célèbre hacker George Hotz, connu pour avoir piraté l’iPhone et la Playstation 3.
+<br/><br/>
+Puisque ces failles sont si précieuses, les entreprises ont tout intérêt à les trouver elles-mêmes. Certaines ont mis en place des modules internes pour traquer les failles. En juillet 2014, Google a lancé Project Zero, une équipe alors dirigée par le « monsieur sécurité » du navigateur Chrome, Chris Evans (qui est aujourd’hui consultant pour HackerOne). Des hackers et spécialistes en sécurité informatique ont pour tâche de traquer les vulnérabilités qui peuvent se cacher dans les produits de Google mais aussi d’autres services. Chaque faille découverte est alors notifiée à l’entreprise concernée, et signalée sur le blog de Project Zero une fois qu’elle a été réparée. L’équipe a notamment compté dans ses rangs le célèbre hacker George Hotz, connu pour avoir piraté l’iPhone et la Playstation 3.<br/><br/>
 
 
 
 
 Quelques exemples de prix:
 
-
-Une faille "Zero-day" dans Vista a été mise en vente 50.000 US$ selon Trend Micro. Le prix moyen de vente d'informations sur de la découverte d'un faille de sécurité non patchée (à exploiter) se situe entre 20.000 US$ et 30.000 US$ selon la popularité du logiciel et la fiabilité du code de l'attaque.
+<br/><br/>
+Une faille "Zero-day" dans Vista a été mise en vente 50.000 US$ selon Trend Micro. Le prix moyen de vente d'informations sur de la découverte d'un faille de sécurité non patchée (à exploiter) se situe entre 20.000 US$ et 30.000 US$ selon la popularité du logiciel et la fiabilité du code de l'attaque.<br/>
 Un zombificateur et son cheval de Troie sont couramment vendus 5.000 US$ selon Gene Raimund (Trend Micro).
-Un trojan sur mesure capable de voler des informations sur des comptes en ligne se trouve entre 1.000 et 5.000 US$.
-Un outil de construction d'un BotNet se trouve entre 5.000 US$ et 20.000 US$.
-En décembre 2005, Kaspersky découvre que l'exploit permettant l'attaque WMF (Windows Metafile) était proposé par un groupe de Hacker Russe pour 4.000 US$. Ces ventes d'exploits se faisant des milliers de fois pour chaque exploit, Gene Raimund (directeur des technologies chez Trend Micro) est allé jusqu'à dire que "Le marché du malware fait plus d'argent que le marché des anti-malwares".
+Un trojan sur mesure capable de voler des informations sur des comptes en ligne se trouve entre 1.000 et 5.000 US$.<br/>
+Un outil de construction d'un BotNet se trouve entre 5.000 US$ et 20.000 US$.<br/>
+En décembre 2005, Kaspersky découvre que l'exploit permettant l'attaque WMF (Windows Metafile) était proposé par un groupe de Hacker Russe pour 4.000 US$. Ces ventes d'exploits se faisant des milliers de fois pour chaque exploit, Gene Raimund (directeur des technologies chez Trend Micro) est allé jusqu'à dire que "Le marché du malware fait plus d'argent que le marché des anti-malwares".<br/><br/>
 
 
 Pour récupérer l’argent, les pirates recrutent des mules. Il s’agit d’intermédiaires qui recevaient de l’argent sur un compte, le plus souvent PayPal, créé pour l’occasion. Même si cela n’est plus possible actuellement. Ensuite, l’argent était converti en cash et transféré vers l’étranger via des services de mandat international comme Western Union. Enfin, il arrive que la mule garde un petit pourcentage du montant d’argent acheminé pour « service rendu ». Les escrocs montent des entreprises fictives pour recruter leurs mules. Ainsi, il n’est pas rare de recevoir des mails – qui finissent le plus souvent dans le courrier indésirable – qui prennent la forme de petites annonces du type « Emploi bien rémunéré pour faible temps de travail ».
+<br/><br/>
 
-
-Partie juridique
-La partie juridique est un sujet sensible quand il s’agit de la sécurité informatique. En effet chaque acteur peut être considéré comme victime ET responsable.
+<t1>Partie juridique:<br/><br/></t1>
+La partie juridique est un sujet sensible quand il s’agit de la sécurité informatique. En effet chaque acteur peut être considéré comme victime ET responsable.<br/><br/>
 
 
 En effet, une entreprise ayant subie une attaque (donc victime) peut être poursuite en justice ! En tant qu’entreprise, elle est garante de la sécurité des données qu’elle heberge : « Le responsable du traitement est tenu de prendre toutes précautions utiles, au regard de la nature des données et des risques présentés par le traitement, pour préserver la sécurité des données et, notamment, empêcher qu’elles soient déformées, endommagées, ou que des tiers non autorisés y aient accès. » (article 34 de loi Informatique et Liberté).
 
-
+<br/><br/>
 Le non respect de ses règles est réprimée par l’article 226-17 du code pénal par cinq ans d’emprisonnement et jusqu’à 300.000€ d’amende.
-
+<br/><br/>
 
 Cependant, une exception s’applique si l’entreprise ne relève pas du statut de responsable du traitement et le sous traitant pourra être mis en cause : « (…) Le sous-traitant doit présenter des garanties suffisantes pour assurer la mise en œuvre des mesures de sécurité et de confidentialité mentionnées à l’article 34. (…) Le contrat liant le sous-traitant au responsable du traitement comporte l’indication des obligations incombant au sous-traitant en matière de protection de la sécurité et de la confidentialité des données et prévoit que le sous-traitant ne peut agir que sur instruction du responsable du traitement. »
-Le chef d’entreprise pourra aussi être sanctionné, s’il refuse de mettre en place les mesures nécessaires pour protéger les données des utilisateurs: « Le fait, y compris par négligence, de procéder ou de faire procéder à des traitements de données à caractère personnel sans qu’aient été respectées les formalités préalables à leur mise en œuvre prévues par la loi ».
-Bien sur, ce genre de cas est très rare, le chef d’entreprise doit s’être réellement obstiné à refuser de mettre en place les mesures nécessaires.
+Le chef d’entreprise pourra aussi être sanctionné, s’il refuse de mettre en place les mesures nécessaires pour protéger les données des utilisateurs: « Le fait, y compris par négligence, de procéder ou de faire procéder à des traitements de données à caractère personnel sans qu’aient été respectées les formalités préalables à leur mise en œuvre prévues par la loi ».<br/>
+Bien sur, ce genre de cas est très rare, le chef d’entreprise doit s’être réellement obstiné à refuser de mettre en place les mesures nécessaires.<br/><br/>
 
 
 Enfin le Directeur du Service Informatique peut être mis en cause si la faille informatique si il en est l’origine. A ce titre il entre dans le cadre de l’article 323-1 : “Le DSI a accédé au système d’information pour une finalité étrangère à son habilitation tout en ayant eu la volonté et la conscience de commettre ce délit “.
